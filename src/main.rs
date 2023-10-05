3252b8a4c9b2a983e3f8ec3abb70cb198ddb703f
Kyle Harrison <redactedprofile@gmail.com>
2023-10-05 07:55:46 -0700

    reading dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 mod common;
 mod days;
 
+use dotenv::dotenv;
+use std::env;
 
 use crate::days::day1 as day1;
 use crate::common::logging::log;
 
 fn main() {
+    dotenv().ok();
+
     log("Hello World");
+
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
 
     day1::test();
 }
