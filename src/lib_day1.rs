617f541c84df2d444fa76a278aaa002c82d6cea9
Kyle Harrison <redactedprofile@gmail.com>
2023-10-05 06:53:56 -0700

    setting up the framework

---
+++ src/lib_day1.rs
@@ -0,0 +1,8 @@
+pub mod lib_day1 {
+
+    pub fn test() -> String {
+        println!("Hello from day 1 test");
+        return String::from("Test");
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod lib_day1;
+
+fn main() {
+    println!("Hello, world!");
+
+    lib_day1::lib_day1::test();
+}
